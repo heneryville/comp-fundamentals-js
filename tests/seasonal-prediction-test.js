0d8a9d32ae040fa3d21b5afae47aa6d635d2f500
Mitchell Harris <mitchellkharris@gmail.com>
2017-08-29 16:46:32 -0600

    Add cdc unit test

--- tests/seasonal-prediction-test.js
+++ tests/seasonal-prediction-test.js
@@ -1,9 +1,10 @@
 const assert = require('chai').assert;
 const seasonalPrediction = require('../seasonal-prediction');
+const fs = require('fs');
+const path = require('path')
 
 describe('seasonal-prediction',function(){
 
-  const twoPointSet = [ [2017,'all year', 10], [2016, 'all year', 9] ];
   const threePointSet = [ [2017,'all year', 10], [2016, 'all year', 9], [2015, 'all year', 2] ];
   const linearGrowth = [
     [2014,'Q1', 10],
@@ -39,17 +40,26 @@ describe('seasonal-prediction',function(){
     [2016,'Q4', 30],
 ];
 
+  const twoPointSet = [ [2017,'all year', 10], [2016, 'all year', 9] ];
   itIs(twoPointSet,2018,'all year', 11,'exact constraint');
   itIs(threePointSet,2018,'all year', 15, 'over constrained');
   itIs(linearGrowth,2018,'Q1', 24.97, 'linearGrowth');
   itIs(linearYearlyGrowthWithSeasonal,2017,'Q1', 40, 'linear Growth with seasonal and on trend');
-  itIs(linearYearlyGrowthWithSeasonal,2017,'Q2', 36, 'linear Growth with seasonal and on belown 10%');
+  itIs(linearYearlyGrowthWithSeasonal,2017,'Q2', 37.333, 'linear Growth with seasonal and on belown 10%');
+
+  const cdc = fs.readFileSync(path.join(__dirname,'../data/cdc-data.csv'),'utf8')
+    .split('\n')
+    .map(x => x.split(',').map(y=> y.trim()))
+    .map(x => [+x[0], x[1], +x[2]])
+
+  itIs(cdc,2017,'45', 910866.998, 'cdc!');
 
 
 
   function itIs(data,year,season,expected,name) {
     it(name, function(){
       let model = seasonalPrediction.learn(data);
+      //console.log(model)
       let actual = seasonalPrediction.predict([year,season],model);
       assert.closeTo(actual,expected,0.001);
     })
